71c9e1c39771bbb5ced0be012ef24b091b2ba548
waiaan <waiaan@163.com>
2022-06-28 11:02:33 +0800

    fix: 1、bug fixed;

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,3 @@
-import Vue from 'vue'
 import ElTreeTransfer from './ElTreeTransfer.vue'
-import '../assets/css/index.scss'
 
-const Components = {
-  ElTreeTransfer
-}
-
-Object.keys(Components).forEach(name => {
-  Vue.component(name, Components[name])
-})
-
-export default Components
+export default ElTreeTransfer
